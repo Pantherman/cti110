51ffd64b2fc7c01043da4df157fd0d3d6a169bd7
Pantherman <31899114+Pantherman@users.noreply.github.com>
2017-09-12 14:00:14 -0400

    Add files via upload
    First submission

---
+++ M2_T1_PenderSr.py
@@ -0,0 +1,9 @@
+# CTI-110 
+# M2T1 - Sales Prediction 
+# Anthony Sr
+# September 11
+#
+#This program calculates annual profit for a company
+
+totalSales = float(input("Enter amount of total sales: "))
+print ('Annual Profit: $', totalSales * 0.23)
