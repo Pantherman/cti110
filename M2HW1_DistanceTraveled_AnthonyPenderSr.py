637799e81beef9f025c68cc59617933696ec9b91
Pantherman <31899114+Pantherman@users.noreply.github.com>
2017-09-12 22:54:48 -0400

    Add files via upload
    Calculate total distance traveled by car going 70mph

---
+++ M2HW1_DistanceTraveled_AnthonyPenderSr.py
@@ -0,0 +1,16 @@
+# CTI-110 
+# M2HW1 - Distance Traveled  
+# Anthony Pender Sr
+# September 12
+
+speed = int(70)
+distanceAfter6 = int(speed * 6)
+distanceAfter10 = int(speed * 10)
+distanceAfter15 = int(speed * 15)
+print ('Distance = speed x time')
+print ('A car is traveling at 70 miles per hour.')
+print ('The distance this care travels in 6 hours is' , distanceAfter6)
+print ('The distance this care travels in 10 hours' , distanceAfter10)
+print ('The distance this care travels in 15 hours' , distanceAfter15)
+
+
