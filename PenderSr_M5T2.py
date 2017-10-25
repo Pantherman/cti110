0c2a1fa62e3a16ce9cbaee57a45a5b16f4b2719b
Pantherman <31899114+Pantherman@users.noreply.github.com>
2017-10-25 12:40:24 -0400

    Add files via upload
    using for loop to count bugs collected in a week

---
+++ PenderSr_M5T2.py
@@ -0,0 +1,20 @@
+# Anthony Pender Sr
+# October 9
+# William Buckwell
+# CTI 110
+
+
+def main():
+
+
+    total = 0
+
+    for day in range (1,8):
+        print ('Enter the bugs collected on day', day)
+        bugs = int(input())
+        total += bugs
+
+    print ('You collected a total of', total, 'bugs.')
+
+main()
+    
