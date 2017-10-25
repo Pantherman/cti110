aa2516d746ce4ceda6ab0f859c8322c015c85f8f
Pantherman <31899114+Pantherman@users.noreply.github.com>
2017-10-25 12:34:12 -0400

    Add files via upload
    Website for Java House

---
+++ index.py
@@ -0,0 +1,48 @@
+<!--
+ CTI 110
+M4HW1 Java House Home Page
+Anthony Pender Sr
+9.27.2017
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>First Tab</title>
+</head>
+<body>
+	<header>
+	<h1>Java House Coffee Shop</h1>
+	</header>
+	<nav>
+		<b>
+		<a href="index.html">Home</a> &nbsp; &nbsp; 
+		<a href="menu.html">Menu</a> &nbsp; &nbsp;
+		<a href="music.html">Music</a> &nbsp; &nbsp; 
+		<a href="jobs.html">Jobs</a>
+		</b>
+	</nav>
+	
+	<main>
+	<h2>Relax at the Java House</h2>
+		<b>Located in historic downtown Fayetteville, Java House has been open since 2007.</b>
+			<ul>
+				<li>Specialty coffee from around the world</li>
+				<li>Muffins, bagels, and sandwiches</li>
+				<li>Live music</li>
+				<li>Open mic Thursday</li>
+			</ul>
+			<div>
+				Java House<br>
+				323 Hay Street<br>
+				Fayetteville, NC 28301<br>
+				(910) 555-7890<br>
+				Open 8am-10pm<br>
+			</div><br>
+	</main>
+	<footer>
+		&copy;&nbsp2017 Java House Inc<br>
+		<a href="mailto:pendera4470@student.faytechcc.edu">My Email</a>	
+	</footer>
+</body>
+</html>
---
+++ menu.py
@@ -0,0 +1,64 @@
+<!--
+ CTI 110
+M4HW1 Java House Menu Page
+Anthony Pender Sr
+9.28.2017
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Menu -- Java House</title>
+</head>
+<body>
+	<header>
+	<h1>Java House Coffee Shop</h1>
+	</header>
+	<nav>
+		<b>
+		<a href="index.html">Home</a> &nbsp; &nbsp; 
+		<a href="menu.html">Menu</a> &nbsp; &nbsp;
+		<a href="music.html">Music</a> &nbsp; &nbsp; 
+		<a href="jobs.html">Jobs</a>
+		</b>
+	</nav>
+	
+	<main>
+	<h2>Coffee and Sandwiches at Java House</h2>
+		<dl>
+			<dt>Coffee</dt>
+				<dd>Regular house blend (Arabica or Robusta),decaf, or our flavor of the day!</dd><br>
+				<dd>Endless Cup $1.99</dd>
+			<dt>Cappuccino</dt>
+				<dd>Espresso with steamed milk. Strong yet subtle.</dd><br>
+				<dd>Single $3.99 Double $4.99</dd>
+			<dt>Iced Coffee</dt>
+				<dd>Our house blend served with ice and milk, perfect for a hot day.</dd><br>
+				<dd>$2.99</dd>
+			<dt>Bagel or Muffin</dt>
+				<dd>From our bakery assortment</dd><br>
+				<dd>$1.49</dd>
+			<dt>Sandwiches</dt>
+				<dd>Your choice of ham, turkey, or sun-dried tomato, on white or wheat. Or try our new hot panini sandwiches!</dd><br>
+				<dd>Regular $3.99 Panini $5.49</dd>
+		</dl>
+		
+		Coffee blends change monthly.<br>
+		We always carry at least one Arabica blend and one Robusta blend.<br>
+		Please call for the latest information.<br>
+		
+			
+		<br><div>
+				Java House<br>
+				323 Hay Street<br>
+				Fayetteville, NC 28301<br>
+				(910) 555-7890<br>
+				Open 8am-10pm<br>
+			</div><br>
+	</main>
+	<footer>
+		&copy;&nbsp2017 Java House Inc<br>
+		<a href="mailto:pendera4470@student.faytechcc.edu">My Email</a>	
+	</footer>
+</body>
+</html>
